ba3bf3f63776fd346d0c6ddb12f12c72fb8de0a1
Chris Tam <chris-tam8@hotmail.com>
2024-03-01 22:25:49 +0800

    add json-core package

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -2,8 +2,7 @@
 // If you want to try std support, also update the guest Cargo.toml file
 #![no_std]  // std support is experimental
 
-
-use risc0_zkvm::guest::env;
+use json_core::Outputs;
 
 risc0_zkvm::guest::entry!(main);
 
