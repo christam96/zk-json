e93ddff9a8f2928f047e1ec17c4691abf362fd2d
Chris Tam <chris-tam8@hotmail.com>
2024-03-01 23:10:27 +0800

    add guest main logic

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,7 +1,8 @@
 #![no_main]
 // If you want to try std support, also update the guest Cargo.toml file
-#![no_std]  // std support is experimental
+// #![no_std]  // std support is experimental
 
+use json::parse;
 use json_core::Outputs;
 use risc0_zkvm::{
     guest::env,
@@ -12,13 +13,47 @@ risc0_zkvm::guest::entry!(main);
 
 
 fn main() {
-    // TODO: Implement your guest code here
+    // Read input JSON from the environment
+    let data: String = env::read();
 
-    // read the input
-    let input: u32 = env::read();
+    // Calculate SHA hash of the input data
+    let sha = *Impl::hash_bytes(&data.as_bytes());
 
-    // TODO: do something with the input
+    // Parse the input data fields
+    let data = parse(&data).unwrap();
 
-    // write public output to the journal
-    env::commit(&input);
+    // Isolate the 'critical_data' field from the parsed JSON
+    let critical_data_check = data["critical_data"].as_u32();
+
+    // Check 'critical_data' field isn't empty, if so, extract it
+    let critical_data = match critical_data_check {
+        Some(value) => value,
+        None => {
+            // Handle the case where 'critical_data' is empty or not present
+            eprintln!("The 'critical_data' field is empty or not present.");
+            // Returning a default value of 0
+            0
+        }
+    };
+
+    // Define the minimum threshold for critical data
+    let threshold_val = 50;
+
+    // Assume the critical data is not proven until evaluated
+    let mut proven = false;
+
+    // Evaluate whether critical data meets the minimum threshold
+    if critical_data > threshold_val {
+        proven = true; // Set proven to true if the condition is met
+    }
+
+    // Define the output struct containing critical data, hash, and evaluation result
+    let out = Outputs {
+        data: critical_data,
+        hash: sha,
+        result: proven,
+    };
+
+    // Commit the output struct to the environment
+    env::commit(&out);
 }
